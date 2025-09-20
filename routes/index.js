8398edd499b64d689b81bfd3e9f91750a4245672
Kashif <kashif147@gmail.com>
2025-09-20 11:09:51 +0100

    update routes/index.js to remove /test-auth route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,5 @@ router.use("/personal-details", require("./personal.details.routes"));
 router.use("/professional-details", require("./professional.details.routes"));
 router.use("/subscription-details", require("./subscription.details.routes"));
 router.use("/applications", require("./application.routes"));
-router.use("/test-auth", require("./test-auth.routes"));
 
 module.exports = router;
