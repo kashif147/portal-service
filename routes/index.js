fa00daec709e729332af1475199cf2d3675fcefd
Kashif <kashif147@gmail.com>
2025-10-10 18:48:55 +0100

    removed /application route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.use("/personal-details", require("./personal.details.routes"));
 router.use("/professional-details", require("./professional.details.routes"));
 router.use("/subscription-details", require("./subscription.details.routes"));
-router.use("/applications", require("./application.routes"));
+// router.use("/applications", require("./application.routes"));
 router.use("/api/application", require("./application.routes"));
 
 module.exports = router;
